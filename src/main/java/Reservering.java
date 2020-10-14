46eb526e74ba6bf3bce5f822903e7557f111cc36
Rein <2197reina@gmail.com>
2020-10-14 11:12:05 +0200

    snapshot voor ik alles omgooi

--- src/main/java/Reservering.java
+++ src/main/java/Reservering.java
@@ -4,14 +4,56 @@
 
 public class Reservering {
     int reserveringsNummer;
-    double tijdsSlot;
+    int tijdsSlot;
+    String naam;
+    int aantalPersonen;
+    Tafel tafel;
 
 
-    public Reservering(int reserveringsNummer, double tijdsSlot){
-        this.reserveringsNummer = reserveringsNummer;
-        this.tijdsSlot = tijdsSlot;
+    public Reservering(int reserveringsNummer, int tijdsSlot, String naam, int aantalPersonen, Tafel tafel){
+        setReserveringsNummer(this.reserveringsNummer);;
+        setTijdsslot(tijdsSlot);
+        setNaam(naam);
+        setAantalPersonen(aantalPersonen);
+        setTafel(tafel);
+
 
     }
 
+
+    private void setReserveringsNummer(int reserveringsNummer) {
+    }
+
+    private int getReserveringsnummer(){
+        return this.reserveringsNummer;
+    }
+
+    private void setTijdsslot(int tijdsSlot) {
+    }
+
+    private int getTijdsslot(){
+        return tijdsSlot;
+    }
+
+    private void setNaam(String naam) {
+    }
+
+    private String getNaam(){
+        return naam;
+    }
+
+    private void setAantalPersonen(int aantalPersonen) {
+    }
+
+    private int getAantalPersonen(){
+        return aantalPersonen;
+    }
+
+    private void setTafel(Tafel tafel) {
+    }
+
+    private Tafel getTafel(){
+        return this.tafel;
+    }
 
 }
